3d0457996e9c5343c8a6c30a05b2e7cac495d68d
Deepsavaliya2507 <deepsavaliya2507@gmail.com>
2023-07-06 08:17:15 +0530

    newsletter change

--- src/components/Footer/Newsletter/Newsletter.jsx
+++ src/components/Footer/Newsletter/Newsletter.jsx
@@ -1,29 +1,65 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
     FaFacebookF,
     FaTwitter,
     FaInstagram,
     FaLinkedinIn,
 } from "react-icons/fa";
+import {
+    addDoc,
+    collection,
+    deleteDoc,
+    doc,
+    getDocs,
+    setDoc,
+} from "firebase/firestore";
+import { fireStoreDb } from "../../../firebaseConfig";
 import "./Newsletter.scss";
 const Newsletter = () => {
 
-    
-
     const [EmailSub, setEmailSub] = useState("");
 
-    const email_data = (e) => {
-        e.preventDefault()
+    useEffect(() => {
+        fetchData();
+    }, []);
 
-        let add = e.target.value;
-        let value = e.target.value;
-        setEmailSub(add);
+    const fetchData = async () => {
+        const querySnapshot = await getDocs(collection(fireStoreDb, "users"));
+        console.log(querySnapshot.docs);
 
-    }
+        const data = [];
+        querySnapshot.forEach((doc) => {
+            if (doc.data().first) {
+                data.push({ id: doc.id, text: doc.data().first });
+            }
+        });
+        setEmailSub(data);
+    };
 
-    const email_subscribe = () => {
-        console.log(EmailSub);
-    }
+    const handleCreate = async () => {
+        // Add a new document in collection "cities"
+        await addDoc(collection(fireStoreDb, "users"), {
+            first: EmailSub,
+            last: "Lovelace",
+            born: 1996,
+        });
+        fetchData();
+        setEmailSub("");
+    };
+
+    const handleUpdate = async (id, newText) => {
+        await setDoc(doc(fireStoreDb, "users", id), {
+            first: newText,
+            last: "Lovelace",
+            born: 1996,
+        });
+        fetchData();
+    };
+
+    const handleDelete = async (id) => {
+        await deleteDoc(doc(fireStoreDb, "users", id));
+        fetchData();
+    };
 
     return (
         <div className="newsletter-section">
@@ -36,9 +72,12 @@ const Newsletter = () => {
                     {/* <input type="text" placeholder="Email Address"
                      onChange={(e => email_log(e))} />
                     <button onClick={() => email_subscribe()}>Subscribe</button> */}
+                    {/* <input type="text" placeholder="Email Address"
+                     onChange={(e => email_data(e))} value={EmailSub} />
+                    <button onClick={() => email_subscribe()} >Subscribe</button> */}
                     <input type="text" placeholder="Email Address"
-                     onChange={(e => email_data(e))} />
-                    <button onClick={() => email_subscribe()} >Subscribe</button>
+                        onChange={(e) => setEmailSub(e.target.value)} value={EmailSub} />
+                    <button onClick={() => handleCreate()} >Subscribe</button>
                 </div>
                 <span className="text">
                     Will be used in accordance with our Privacy Policy
