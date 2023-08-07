e90a90309533a843461e27135e8223fc70363c21
Deepsavaliya2507 <deepsavaliya2507@gmail.com>
2023-08-07 08:27:53 +0530

    new changes

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,139 +1,132 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { MdClose } from "react-icons/md";
 import { BsCartX } from "react-icons/bs";
 import { Context } from "../../utils/context";
 import CartItem from "./CartItem/CartItem";
 // import { loadStripe } from "@stripe/stripe-js";
 import { makePaymentRequest } from "../../utils/api";
-import {
-    collection,
-    getDocs,
-  } from "firebase/firestore";
-  import { fireStoreDb } from "../../firebaseConfig";
+import { collection, getDocs } from "firebase/firestore";
+import { fireStoreDb } from "../../firebaseConfig";
 
 import "./Cart.scss";
 
 const Cart = () => {
-    const { myCart, setShowCart, cartSubTotal } = useContext(Context);
+  const { myCart, setShowCart, cartSubTotal } = useContext(Context);
+  const [data, setData] = useState([]);
 
-    useEffect(() => {
-        fetchData();
-    }, []);
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    const fetchData = async () => {
-        const querySnapshot = await getDocs(collection(fireStoreDb, "myCarts"));
-    
-        const data = [];
-        querySnapshot.forEach((doc) => {
-          if (doc.name) {
-            data.push({
-              id: 1234,
-              name: doc().name,
-              price: doc().price,
-              image: doc().image,
-            });
-          }
+  const fetchData = async () => {
+    const querySnapshot = await getDocs(collection(fireStoreDb, "addproducts"));
+
+    const data = [];
+    querySnapshot.forEach((doc) => {
+      if (doc.data().name) {
+        data.push({
+          id: doc.id,
+          name: doc.data().productname,
+          price: doc.data().Price,
+          image: doc.data().Image,
         });
-        // setData(data);
+      }
+    });
+    setData(data);
+  };
+
+  // const stripePromise = loadStripe(
+  //     process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY
+  // );
+
+  const handlePayment = async () => {
+    try {
+      // const stripe = await stripePromise;
+      const res = await makePaymentRequest.post("/api/orders", {
+        products: myCart,
+      });
+      await {
+        // sessionId: res.data.stripeSession.id,
+        sessionId: res.data.id,
       };
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-    // const stripePromise = loadStripe(
-    //     process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY
-    // );
-    
-
-    const handlePayment = async () => {
-        try {
-            // const stripe = await stripePromise;
-            const res = await makePaymentRequest.post("/api/orders", {
-                products: myCart,
-            });
-            await ({
-                // sessionId: res.data.stripeSession.id,
-                sessionId: res.data.id,
-            });
-        } catch (err) {
-            console.log(err);
-        }
-    };
-
-    return (
-        <div className="cart-panel">
-            <div
-                className="opac-layer"
-                onClick={() => setShowCart(false)}
-            ></div>
-            <div className="cart-content">
-                <div className="cart-header">
-                    <span className="heading">Shopping Cart</span>
-                    <span
-                        className="close-btn"
-                        onClick={() => setShowCart(false)}
-                    >
-                        <MdClose className="close-btn" />
-                        <span className="text">close</span>
-                    </span>
-                </div>
-
-                {!myCart.length && (
-                    <div className="empty-cart">
-                        <BsCartX />
-                        <span>No products in the cart.</span>
-                        <button className="return-cta" onClick={() => {}}>
-                            RETURN TO SHOP
-                        </button>
-                    </div>
-                )}
-
-                {/* {!!cartItems.length && (
-                    <>
-                        <CartItem />
-                        <div className="cart-footer">
-                            <div className="subtotal">
-                                <span className="text">Subtotal:</span>
-                                <span className="text total">
-                                    &#8377;{cartSubTotal}
-                                </span>
-                            </div>
-                            <div className="button">
-                                <button
-                                    className="checkout-cta"
-                                    onClick={handlePayment}
-                                >
-                                    Checkout
-                                </button>
-                            </div>
-                        </div>
-                    </>
-                )} */}
-
-                {!!myCart.length && (
-                    <>
-                        <CartItem />
-                        <div className="cart-footer">
-                            <div className="subtotal">
-                                <span className="text">Subtotal:</span>
-                                <span className="text total">
-                                    &#8377;{cartSubTotal}
-                                </span>
-                            </div>
-                            <div className="button">
-                                <button
-                                    className="checkout-cta"
-                                    onClick={handlePayment}
-                                >
-                                    Checkout
-                                </button>
-                            </div>
-                        </div>
-                    </>
-                )}
-
-                {/* {myCarts} */}
-
-            </div>
+  return (
+    <div className="cart-panel">
+      <div className="opac-layer" onClick={() => setShowCart(false)}></div>
+      <div className="cart-content">
+        <div className="cart-header">
+          <span className="heading">Shopping Cart</span>
+          <span className="close-btn" onClick={() => setShowCart(false)}>
+            <MdClose className="close-btn" />
+            <span className="text">close</span>
+          </span>
         </div>
-    );
+
+        {!myCart && (
+          <div className="empty-cart">
+            {/* <BsCartX /> */}
+            {data.map((item) => (
+              <div className="flex">
+                <img
+                  src={item.image}
+                  alt=""
+                  srcset=""
+                  height={200}
+                  width={100}
+                />
+                <p>Name : {item.name}</p>
+                <p>Price : {item.price}</p>
+              </div>
+            ))}
+            <span>No products in the cart.</span>
+            <button className="return-cta" onClick={() => {}}>
+              RETURN TO SHOP
+            </button>
+          </div>
+        )}
+
+        {!!CartItem && (
+          <>
+            <CartItem />
+            <div className="cart-footer">
+              <div className="subtotal">
+                <span className="text">Subtotal:</span>
+                <span className="text total">&#8377;{cartSubTotal}</span>
+              </div>
+              <div className="button">
+                <button className="checkout-cta" onClick={handlePayment}>
+                  Checkout
+                </button>
+              </div>
+            </div>
+          </>
+        )}
+
+        {!!myCart && (
+          <>
+            <CartItem />
+            <div className="cart-footer">
+              <div className="subtotal">
+                <span className="text">Subtotal:</span>
+                <span className="text total">&#8377;{cartSubTotal}</span>
+              </div>
+              <div className="button">
+                <button className="checkout-cta" onClick={handlePayment}>
+                  Checkout
+                </button>
+              </div>
+            </div>
+          </>
+        )}
+
+        {/* {myCarts} */}
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -44,169 +44,67 @@ const Product = () => {
     fetchData();
   }, []);
 
-  const handleCreate = async () => {
-    if (productname && Price && Image) {
-      await addDoc(collection(fireStoreDb, "products"), {
-        name: productname,
-        image: Image,
-        price: Price,
-      });
-      fetchData();
-      setProductname("");
-      setProductPrice("");
-      setProductImage("");
-    }
-    // Add a new document in collection "cities"
-  };
-
-  const handleUpdate = async (id, productname, Price , Image) => {
-    if (productname && Price && Image) {
-      await setDoc(doc(fireStoreDb, "products", id), {
-        name: productname,
-        image: Image,
-        price: Price,
-      });
-      fetchData();
-    }
-    console.log(productname);
-  };
-
   const handleDelete = async (id) => {
     await deleteDoc(doc(fireStoreDb, "products", id));
     fetchData();
   };
 
-  const handleLike = async (data) => {
-    await addDoc(collection(fireStoreDb, "likeProducts"), {
-      name: data.name,
-      image: data.image,
-      price: data.price,
+  const handleLike = async () => {
+    // if(productname && Price && Image){
+    await addDoc(collection(fireStoreDb, "addproducts"), {
+      name: productname,
+      image: Image,
+      price: Price,
     });
+    fetchData();
+    // }
   };
 
   return (
     <div>
-      <h2>CRUD App with Firebase</h2>
-      <input
-        type="text"
-        style={{
-          border: "2px solid black",
-          padding: "10px 30px",
-          marginBottom: "10px",
-        }}
-        placeholder="Please add Product Image"
-        value={Image}
-        onChange={(e) => setProductImage(e.target.value)}
-      />
-      <br />
-      <input
-        type="text"
-        style={{ border: "2px solid black", padding: "10px 30px" }}
-        placeholder="Please add Product Name"
-        value={productname}
-        onChange={(e) => setProductname(e.target.value)}
-      />
-      <br />
-      <input
-        type="number"
-        style={{
-          border: "2px solid black",
-          margin: "10px 0px",
-          padding: "10px 30px",
-        }}
-        placeholder="Please add Product Price"
-        value={Price}
-        onChange={(e) => setProductPrice(e.target.value)}
-      />
-      <br />
-      <button
-        onClick={() => {
-          handleCreate();
-        }}
-        style={{
-          padding: "10px 30px",
-          border: "2px solid black",
-          backgroundColor: "wheat",
-          margin: "0px 50px",
-          borderRadius: "5px",
-        }}
-      >
-        Add Product
-      </button>
-      <ul>
+      <ul className="flex grid grid-cols-4">
         {data.map((item) => (
           <li key={item.id}>
-            <div className="pt-12 pb-12 flex justify-around border-black">
-              <div className="max-w-sm relative rounded mr-4 w-1/4 overflow-hidden shadow-lg">
-                <div className="grid justify-items-center">
-                  <img
-                    src={item.image}
-                    alt=""
-                    srcset=""
-                    height={500}
-                    width={300}
-                  />
-                </div>
-
-                <span className="inline-block text-sm font-semibold">
-                  <input
-                    className="px-3 py-1 rounded-full text-xl text-black font-semibold hover:text-black
-               hover:bg-white focus:outline-none hover:text-bg-black relative"
-                    // onClick={() => handleCreate()}
-                    type="text"
-                    placeholder="Update Product Name"
-                    defaultValue={item.name}
-                    onChange={(e) => setupdateProductName(e.target.value)}
-                  />
-                  <p>{updateProductName}</p>
-                  <span className=" absolute top-0 right-0">
-                    <i className="fa-regular fa-heart"></i>
-                  </span>
-                  <span className="icon-space absolute top-0 right-0" required>
-                    <i className="fa-solid fa-heart text-red-600"></i>
-                  </span>
-                </span>
-
-                <div className="px-6 py-4">
-                  <div className="font-bold text-xl mb-2">
-                    <input
-                      type="number"
-                      placeholder="Update Product Price"
-                      defaultValue={item.price}
-                      onChange={(e) => setupdateProductPrice(e.target.value)}
-                    />
+            <div>
+              <div className="pt-12 pb-12 flex justify-around border-black">
+                <div className="max-w-sm relative rounded mr-4 w-1/4 overflow-hidden shadow-lg widthsize">
+                  <div className="grid justify-items-center">
+                    <img src={item.image} className="rounded imgchange" />
+                  </div>
+                  <span className="inline-block absolute top-0 right-0 text-sm font-semibold"></span>
+                  <div className="px-6 py-4">
+                    <div className="font-bold text-xl mb-2">
+                      <div className="form">
+                        <p>Name : {item.name}</p>
+                      </div>
+                    </div>
+                    <p>Price : {item.price}</p>
+                    <p className="text-gray-700 text-base">
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                      Voluptatibus quia, nulla! Maiores et perferendis eaque,
+                      exercitationem praesentium nihil.
+                    </p>
+                  </div>
+                  <div className="px-6 pt-4 pb-2">
+                    <span className="inline-block pr-2 rounded- text-sm font-semibold">
+                      <button
+                        className="px-2 py-1 rounded-full text-sm text-blue-700 font-semibold border border-blue-700 hover:text-black
+               hover:bg-white hover:border-black focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2"
+                      >
+                        Buy it Now
+                      </button>
+                    </span>
+                    <span className="inline-block rounded-full text-sm font-semibold">
+                      <button
+                        onClick={() => handleLike(item)}
+                        className="px-2 py-1 rounded-full text-sm text-blue-700 font-semibold border border-blue-700 hover:text-black
+               hover:bg-white hover:border-black focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2"
+                      >
+                        Add to Cart
+                      </button>
+                    </span>
                   </div>
                 </div>
-
-                <div className="px-6 py-4">
-                  <div className="font-bold text-xl mb-2">
-                    <button
-                      className="border border-blue-500 p-3 mr-2"
-                      onClick={() =>
-                        handleUpdate(
-                          item.id,
-                          updateProductName,
-                          updatedProductPrice
-                        )
-                      }
-                    >
-                      Update Product
-                    </button>
-                  </div>
-                </div>  
-
-                <button
-                  className="border border-blue-500 p-3 mr-2"
-                  onClick={() => handleDelete(item.id)}
-                >
-                  Delete
-                </button>
-                <button
-                  className="border border-blue-500 p-3 hover:"
-                  onClick={() => handleLike(item)}
-                >
-                  Like
-                </button>
               </div>
             </div>
           </li>
